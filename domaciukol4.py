0f80c60404702df7f3a42d80874fa67a7f5d604c
Olga KK <olgakk@volny.cz>
2023-10-24 23:46:03 +0200

    4.domaci ukol

--- domaciukol4.py
+++ domaciukol4.py
@@ -1,5 +1,5 @@
 def overenicisla(telcislo):
-    if len(telcislo) == 9 or (telcislo[:4] == "+420" and len(telcislo) == 13):
+    if len(telcislo)==9 or (telcislo[:4]=="+420" and len(telcislo)==13):
         return True
     else:
         return False
@@ -8,7 +8,7 @@ def vypocetcenyzpravy(zprava):
     return cena
 telcislo = input("Napiste telefonni cislo:")
 if overenicisla(telcislo):
-    zprava = input("Napiste text zpravy:")
+    zprava=input("Napiste zpravu:")
     delka=len(zprava)+179
     cena = vypocetcenyzpravy(zprava)
     print(f"Cena zpravy je {cena} Kc.")
